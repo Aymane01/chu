eb5c89f03904ee1781fbc25c96acbfb218e07bb1
Aymane01 <aymane.hammioui@gmail.com>
2017-08-26 14:07:53 +0100

    SQL

--- resources/bdCHU.sql
+++ resources/bdCHU.sql
@@ -47,3 +47,35 @@ echelon INT,
 indice INT
 ) 
 go
+
+CREATE TABLE Resident(
+cin VARCHAR(10) PRIMARY KEY,
+ppr INT,
+nomFr VARCHAR(50),
+prenomFr VARCHAR(50),
+nomCompletAr VARCHAR(100) COLLATE Arabic_100_CS_AS_KS_WS_SC,
+dateN DATE,
+sexe VARCHAR(10),
+nationalite VARCHAR(50),
+dateArrive DATE,
+budget VARCHAR (50),
+observation VARCHAR (100),
+echelle INT,
+echelon INT,
+indice INT,
+idCorps INT,
+idGrade INT,
+idCadre INT,
+idService INT,
+idSpecialite INT,
+status VARCHAR (100),
+typeR VARCHAR (100),
+
+	 
+CONSTRAINT  fk_corps FOREIGN KEY (idCorps) REFERENCES Corps(idCr),
+CONSTRAINT  fk_grade FOREIGN KEY (idGrade) REFERENCES Grade(idG),
+CONSTRAINT  fk_cadre FOREIGN KEY (idCadre) REFERENCES Cadre(idCa),
+CONSTRAINT  fk_service FOREIGN KEY (idService) REFERENCES Service(idSe),
+CONSTRAINT  fk_specialite FOREIGN KEY (idSpecialite) REFERENCES Specialite(idS)
+)
+go
